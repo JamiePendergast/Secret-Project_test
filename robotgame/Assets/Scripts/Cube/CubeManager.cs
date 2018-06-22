05e2d6299b73d4c58b0d6295e4912dc003a2bf82
Jamie Pendergast <jamiependergast@gmail.com>
2018-06-22 12:13:47 +0100

    First commit
    Yay!

---
+++ robotgame/Assets/Scripts/Cube/CubeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube
+{
+    public sealed class CubeManager : MonoBehaviour
+    { 
+        private List<CubeObject> cubeObjects = new List<CubeObject>();
+        private CubeObject self;
+
+        private void SetSelf(ref CubeObject obj)
+        {
+            self = obj;
+        }
+
+        private void Start()
+        {
+            CubeObject cubeObject = GetComponent<CubeObject>();
+            SetSelf(ref cubeObject);    
+        }
+
+        public CubeObject PlaceCube(CubeObject prefab, Vector3 where, Quaternion rotation)
+        {
+            CubeObject cubeObject = Instantiate<CubeObject>(prefab, where, rotation, transform) as CubeObject;
+            for (int connectionPoint = 0; connectionPoint < prefab.ConnectionPoints.Count - 1; connectionPoint++)
+                cubeObject.AttachmentPoints.Add(cubeObject.transform.TransformPoint(prefab.ConnectionPoints[connectionPoint]));
+            cubeObjects.Add(cubeObject);
+            return cubeObject;
+        }   
+
+        public CubeObject GetCube(Vector3 where)
+        {
+            where = transform.InverseTransformPoint(where);
+            for(int index = 0; index < cubeObjects.Count - 1; index++)
+            {
+                if (cubeObjects[index].transform.position == where)
+                    return cubeObjects[index];
+            }
+            return null;
+        }
+
+        public void DestroyCube(Vector3 where)
+        {
+            CubeObject cube = GetCube(where);
+            if (cube != null)
+                Destroy(cube.gameObject);
+        }
+    }
+}
---
+++ robotgame/Assets/Scripts/Cube/CubeObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube
+{
+    public sealed class CubeObject : MonoBehaviour
+    {
+        private List<CubeObject> connectedCubes = new List<CubeObject>();
+        public List<Vector3> ConnectionPoints = new List<Vector3>();
+        public List<Vector3> AttachmentPoints = new List<Vector3>();
+
+        public MeshRenderer[] GetRenderers()
+        {
+            return GetComponentsInChildren<MeshRenderer>(true);
+        }
+
+        public MeshFilter[] GetMeshFilters()
+        {
+            return GetComponentsInChildren<MeshFilter>(true);
+        }
+
+        public List<Material> GetMaterials()
+        {
+            List<Material> materials = new List<Material>();
+            var renderers = GetRenderers();
+
+            for (int rendererIndex = 0; rendererIndex < renderers.Length - 1; rendererIndex++)
+            {   
+                for(int materialIndex = 0; materialIndex < renderers[rendererIndex].materials.Length; materialIndex++)
+                {
+                    materials.Add(renderers[rendererIndex].materials[materialIndex]);
+                }
+            }
+
+            return materials;
+        }
+    }
+}
